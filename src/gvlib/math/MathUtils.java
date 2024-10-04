921d85eb6d65cd361ab160993e0fc9098ece06c9
Gerardo <gmatiasvc@hotmail.com>
2024-10-04 15:30:32 -0500

    added compundInterese method to MathUtils

--- src/gvlib/math/MathUtils.java
+++ src/gvlib/math/MathUtils.java
@@ -226,14 +226,24 @@ public class MathUtils {
     }
 
     public static double simpleInterest(float m, float p, int c){
-        if (c < 2){
+        if (c < 1){
             return -1;
         }
         double result=m;
         for (int i = 1; i <= c; i++) {
             result += m*p;
         }
-
         return result;
     } 
+
+    public static double compoundInterest(float m, float p, int c){
+        if (c < 1){
+            return -1;
+        }
+        double result=m;
+        for (int i = 1; i <= c; i++) {
+            result += result*p;
+        }
+        return result;
+    }
 }
